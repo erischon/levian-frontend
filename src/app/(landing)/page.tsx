eb2572edb8313745311e04b3bdb4d566be4811d1
Eri Schon <erischon@gmail.com>
2023-07-20 08:51:56 +0200

    project

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <main className="w-full h-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-semibold">Bienvenue sur Levian !</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-2xl font-semibold">Bienvenue sur Levian !</h1>
-    </main>
-  );
-}
