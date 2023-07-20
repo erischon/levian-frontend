81cee17d71a46e7c800eeea82c2d65db3916e4b1
Eri Schon <erischon@gmail.com>
2023-07-20 10:15:55 +0200

    added register

--- src/components/ProjectForm/FormRow.tsx
+++ src/components/ProjectForm/FormRow.tsx
@@ -3,21 +3,39 @@ interface FormRowProps {
   type: string;
   name: string;
   children?: React.ReactNode;
+  register: any;
 }
 
-const FormRow: React.FC<FormRowProps> = ({ label, type, name, children }) => {
+const FormRow: React.FC<FormRowProps> = ({
+  label,
+  type,
+  name,
+  children,
+  register,
+}) => {
   return (
     <>
       <div className="flex flex-col mb-2">
         <label htmlFor={name} className="text-sm">
           {label}
         </label>
+
         {type === "select" ? (
-          <select name={name} id={name} className="block border">
+          <select
+            name={name}
+            id={name}
+            className="block border"
+            {...register(`${name}`)}
+          >
             {children}
           </select>
         ) : (
-          <input id={name} type={type} className="block border" />
+          <input
+            id={name}
+            type={type}
+            className="block border"
+            {...register(`${name}`)}
+          />
         )}
       </div>
     </>
--- src/components/ProjectForm/ProjectForm.tsx
+++ src/components/ProjectForm/ProjectForm.tsx
@@ -7,16 +7,28 @@ import FormRow from "./FormRow";
 const ProjectForm = () => {
   const form = useForm();
 
+  const { register } = form;
+
   return (
     <>
       <form className="flex flex-col w-full sm:max-w-xl px-6 py-6 mx-auto mb-6 bg-slate-50 rounded-md">
-        <FormRow label="Nom du projet" type="text" name="name" />
+        <FormRow
+          label="Nom du projet"
+          type="text"
+          name="name"
+          register={register}
+        />
 
-        <FormRow label="Client" type="text" name="client" />
+        <FormRow label="Client" type="text" name="client" register={register} />
 
-        <FormRow label="Date de début" type="date" name="startDate" />
+        <FormRow
+          label="Date de début"
+          type="date"
+          name="startDate"
+          register={register}
+        />
 
-        <FormRow label="Status" type="select" name="status">
+        <FormRow label="Status" type="select" name="status" register={register}>
           <option>In progress</option>
           <option>Completed</option>
           <option>Canceled</option>
