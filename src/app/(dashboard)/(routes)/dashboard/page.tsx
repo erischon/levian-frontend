22cf2b180f01d0a105a58f4f72402145c432d2ee
Eri Schon <erischon@gmail.com>
2023-07-21 14:09:37 +0200

    refactoring button

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 
+import { AiOutlineProject } from "react-icons/ai";
+import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
+
 import Button from "@/components/Button";
 import CardProject from "@/components/CardProject";
 
@@ -25,14 +28,26 @@ const DashboardPage = async (): Promise<JSX.Element> => {
 
   return (
     <>
-      <section className="my-6">
+      <section className="flex gap-4 my-6">
+        <Button color={"bg-teal-700"}>
+          <div className="flex gap-2 justify-between items-center">
+            <HiOutlineBuildingOffice2 />
+
+            <Link href="/project">Add Client</Link>
+          </div>
+        </Button>
+
         <Button>
-          <Link href="/project">Add Project</Link>
+          <div className="flex gap-2 justify-between items-center">
+            <AiOutlineProject />
+
+            <Link href="/project">Add Project</Link>
+          </div>
         </Button>
       </section>
 
       <section className="">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {data.map((project: any) => (
             <CardProject key={project._id} project={project} />
           ))}
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 interface ButtonProps {
+  color?: string;
   children: React.ReactNode;
 }
 
@@ -6,9 +7,13 @@ interface ButtonProps {
  * @description Button component
  * @returns {JSX.Element}
  */
-const Button: React.FC<ButtonProps> = ({ children }): JSX.Element => {
+const Button: React.FC<ButtonProps> = ({ children, color }): JSX.Element => {
   return (
-    <button className="my-2 px-6 py-2 bg-indigo-600 text-white font-semibold rounded-md">
+    <button
+      className={`my-2 px-6 py-2 ${
+        color ? color : "bg-indigo-700"
+      } text-white font-semibold rounded-md shadow-sm hover:opacity-80 transition-opacity duration-300 ease-in-out`}
+    >
       {children}
     </button>
   );
