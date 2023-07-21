6606d08721c3d5f3e67922ef577018ea2af58587
Eri Schon <erischon@gmail.com>
2023-07-21 14:22:26 +0200

    refactoring - custom displayed data

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -24,7 +24,29 @@ async function getProjects() {
  * @returns {JSX.Element}
  */
 const DashboardPage = async (): Promise<JSX.Element> => {
-  const data = await getProjects();
+  let data = await getProjects();
+
+  // sort projects by date
+  function sortByDate(data: any) {
+    data.sort((a: any, b: any) => {
+      return new Date(b.startDate).getTime() - new Date(a.startDate).getTime();
+    });
+  }
+
+  // only show 3 projects
+  function showThreeProjects(data: any) {
+    return data.slice(0, 5);
+  }
+
+  // only show project with status "in_progress"
+  function showInProgressProjects(data: any) {
+    return data.filter((project: any) => project.status === "in_progress");
+  }
+
+  // Configure data
+  sortByDate(data);
+  data = showThreeProjects(data);
+  data = showInProgressProjects(data);
 
   return (
     <>
